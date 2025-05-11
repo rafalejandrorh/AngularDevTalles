3c3be2d777c23b9a857a196bee43c5d48247e8b9
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-11 15:47:21 -0400

    @for Elements Iteration

--- 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball',
@@ -7,5 +13,11 @@ import { Component } from '@angular/core';
   styleUrl: './dragonball-page.component.css'
 })
 export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 15000 },
+    { id: 2, name: 'Vegeta', power: 13000 },
+    { id: 3, name: 'Gohan', power: 12000 }
+  ]);
 
 }
