5dc53ca944663746b4826c272159c0bd627e3445
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-11 15:59:09 -0400

    Apply NgClass and Conditionals for CSS/Bootstrap Classes

--- 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,16 +9,24 @@ interface Character {
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
   templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css'
+  styleUrl: './dragonball-page.component.css',
+  imports: [
+    NgClass
+  ],
 })
 export class DragonballPageComponent {
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 13000 },
-    { id: 3, name: 'Gohan', power: 12000 }
+    { id: 3, name: 'Gohan', power: 9000 }
   ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': true
+    }
+  });
 
 }
