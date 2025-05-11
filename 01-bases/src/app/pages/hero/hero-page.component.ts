2f19bfada3120d951a6e36d114a9127b05b0a307
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-11 08:56:45 -0400

    Refactor Hero Page Component

--- 01-bases/src/app/pages/hero/hero-page.component.ts
+++ 01-bases/src/app/pages/hero/hero-page.component.ts
@@ -7,37 +7,37 @@ import { Component, signal } from '@angular/core';
 })
 export class HeroPageComponent {
 
-    nameSignal = signal('Ironman');
-    ageSignal = signal(45);
+  nameSignal = signal('Ironman');
+  ageSignal = signal(45);
 
-    // capitalizedName(name: string):string {
-    //     return name.toUpperCase();
-    // }
+  // capitalizedName(name: string):string {
+  //     return name.toUpperCase();
+  // }
 
-    name():string {
-        return this.nameSignal();
-    }
+  name():string {
+    return this.nameSignal();
+  }
 
-    age():number {
-        return this.ageSignal();
-    }
+  age():number {
+    return this.ageSignal();
+  }
 
-    getHeroDescription():string {
-        return `${this.nameSignal()} - ${this.ageSignal()}`;
-    }
+  getHeroDescription():string {
+    return `${this.nameSignal()} - ${this.ageSignal()}`;
+  }
 
-    changeHero():void {
-        this.nameSignal.set('Spiderman');
-        this.ageSignal.set(22);
-    }
+  changeHero():void {
+    this.nameSignal.set('Spiderman');
+    this.ageSignal.set(22);
+  }
 
-    changeAge():void {
-        this.ageSignal.set(60);
-    }
+  changeAge():void {
+    this.ageSignal.set(60);
+  }
 
-    resetForm():void {
-        this.nameSignal.set('Ironman')
-        this.ageSignal.set(45)
-    }
+  resetForm():void {
+    this.nameSignal.set('Ironman')
+    this.ageSignal.set(45)
+  }
 
 }
