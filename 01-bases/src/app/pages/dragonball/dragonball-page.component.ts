2ad740d4bd42d2eb6bfc6b51bc5c5d278b40f74f
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-11 15:38:49 -0400

    Dragonball Page Component

---
+++ 01-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css'
+})
+export class DragonballPageComponent {
+
+}
