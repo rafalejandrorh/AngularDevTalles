fa4ee866c79dce74ce1abb320edbec84d4e9268f
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-12 18:24:08 -0400

    Refactor Dragonball super page component

--- 01-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 01-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,43 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { Character } from '../../interfaces/character.interface';
+import { CharacterAddComponent } from "../../components/dragonball/character-add/character-add.component";
 
 @Component({
   selector: 'app-dragonball',
   templateUrl: './dragonball-super-page.component.html',
   styleUrl: './dragonball-super-page.component.css',
   imports: [
-    CharacterListComponent
-  ],
+    CharacterListComponent,
+    CharacterAddComponent
+],
 })
 export class DragonballSuperPageComponent {
-
-  name = signal('');
-  power = signal(0);
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 13000 }
   ]);
-
-
-  addCharacter() {
-    //console.log(this.name(), this.power());
-    if(!this.name() || !this.power() || this.power() <= 0) return;
-
-    const newCharacter: Character = {
-      id: this.characters().length + 1,
-      name: this.name(),
-      power: this.power()
-    }
-    this.characters.update(characters => [...characters, newCharacter]);
-    
-    this.resetFields();
-  }
-  
-  resetFields() {
-    this.name.set('');
-    this.power.set(0);
-  }
 
 }
