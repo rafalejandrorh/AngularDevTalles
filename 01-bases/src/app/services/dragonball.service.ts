34023f30e981e76427209828b79400aaf58d75ff
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-12 19:50:40 -0400

    Use Effects and Local Sotrage

--- 01-bases/src/app/services/dragonball.service.ts
+++ 01-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
 @Injectable({providedIn: 'root'})
@@ -8,6 +8,10 @@ export class DragonballService {
     { id: 1, name: 'Goku', power: 15000 },
     { id: 2, name: 'Vegeta', power: 13000 }
   ]);
+
+  saveToLocalStorage = effect( () => {
+    localStorage.setItem('characters', JSON.stringify(this.characters()));
+  });
 
   addCharacter(character: Character) {
     console.log(character);
