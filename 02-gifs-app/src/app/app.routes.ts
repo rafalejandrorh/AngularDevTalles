58fe38718cd624fd795850219c2c0561a296a52a
Rafael Rivero <rafalejandrorivero@gmail.com>
2025-05-13 09:51:09 -0400

    Use Load Lazy Component

--- 02-gifs-app/src/app/app.routes.ts
+++ 02-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+//import { DashboardPageComponent } from './gifs/pages/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'dashboard',
+        loadComponent: () =>  import('./gifs/pages/dashboard-page/dashboard-page.component')
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
