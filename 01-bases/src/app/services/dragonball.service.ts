aa6cbb07f3b813a8590de2be867d4d8744def213
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-12 20:01:41 -0400

    Use Linked Signal - Get Local Storage

--- 01-bases/src/app/services/dragonball.service.ts
+++ 01-bases/src/app/services/dragonball.service.ts
@@ -1,13 +1,15 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+};
+
 @Injectable({providedIn: 'root'})
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 15000 },
-    { id: 2, name: 'Vegeta', power: 13000 }
-  ]);
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   saveToLocalStorage = effect( () => {
     localStorage.setItem('characters', JSON.stringify(this.characters()));
