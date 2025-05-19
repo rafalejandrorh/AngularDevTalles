0a55a18af570a3c0037c4c868dc873028077289f
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-19 12:00:31 -0400

    Gif List Component showing images

---
+++ 02-gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ListItemComponent } from "./list-item/list-item.component";
+
+@Component({
+  selector: 'gif-list',
+  imports: [ListItemComponent],
+  templateUrl: './gif-list.component.html',
+})
+export class GifListComponent { }
---
+++ 02-gifs-app/src/app/gifs/components/gif-list/list-item/list-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-list-item',
+  imports: [],
+  templateUrl: './list-item.component.html',
+})
+export class ListItemComponent { }
