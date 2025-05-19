1308b1eee790dbbc834895eed3c1905b0e305afa
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-19 16:18:46 -0400

    Gif Mapper Implementation

--- 02-gifs-app/src/app/gifs/services/gif.service.ts
+++ 02-gifs-app/src/app/gifs/services/gif.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
+import { Gif } from '../interfaces/gif.interface';
+import { GifMapper } from '../mapper/gif.mapper';
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +12,22 @@ export class GifService {
 
   private http = inject(HttpClient);
 
+  trendingGifs = signal<Gif[]>([]);
+
   constructor() {
     this.loadTrendingGifs();
   }
 
   loadTrendingGifs() {
-    
-    this.http.get<GiphyResponse>(`${environment.giphy.url}/gifs/trending/`, {
+    this.http.get<GiphyResponse>(`${environment.giphy.url.v1}/gifs/trending/`, {
       params: {
         api_key: environment.giphy.apiKey
       }
+    }).subscribe((response) => {
+      const gifs = GifMapper.mapGiphyItemsToGifArray(response.data);
+      this.trendingGifs.set(gifs);
+      console.log(gifs);
     })
-
   }
 
 }
