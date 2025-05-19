bf5c32e14d64c71bd60d42fc6abc2879e114e324
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-19 19:11:40 -0400

    Show History Search

--- 02-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
+++ 02-gifs-app/src/app/gifs/pages/gif-history/gif-history.component.ts
@@ -1,19 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
+import { GifService } from '../../services/gif.service';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'gif-history',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './gif-history.component.html',
 })
 export default class GifHistoryComponent { 
   
+  gifService = inject(GifService);
+
   query = toSignal(
     inject(ActivatedRoute).params.pipe(
       map(params => params['query'])
     )
   );
+
+  gifsByKey = computed(() => this.gifService.getHistoryGifs(this.query()));
 
 }
