174a70d6a83b3ff1343d959c9982b23ca5577f23
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-19 18:31:33 -0400

    Show Previous History Search

--- 02-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ 02-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { GifService } from 'src/app/gifs/services/gif.service';
 
 interface MenuOption {
   label: string;
@@ -14,6 +15,8 @@ interface MenuOption {
   templateUrl: './side-menu-options.component.html',
 })
 export class SideMenuOptionsComponent {
+
+  gifService = inject(GifService);
 
   menuOptions: MenuOption[] = [
     {
