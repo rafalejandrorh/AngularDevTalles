6d1631147d38a0415d5d0747e8f12c85d8c9e383
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-19 11:35:02 -0400

    Router Links Side Menu Options

--- 02-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ 02-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,8 +1,37 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  icon: string;
+  route: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
-export class SideMenuOptionsComponent { }
+export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    }
+  ];
+
+  constructor() {
+    this.menuOptions;
+  } 
+
+}
