0cc66b74dfad69e44296a8ec6b3f656a75784838
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-20 15:59:38 -0400

    determine scroll end

--- 02-gifs-app/src/app/gifs/components/gif-list/random-list/random-list.component.ts
+++ 02-gifs-app/src/app/gifs/components/gif-list/random-list/random-list.component.ts
@@ -10,8 +10,15 @@ export class RandomListComponent {
   gifsGroup = input.required<Gif[][]>();
 
   scrollDivRef = viewChild<ElementRef>('groupDiv');
+
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
     if(!scrollDiv) return;
+    const scrollTop = scrollDiv.scrollTop;
+    const scrollHeight = scrollDiv.scrollHeight;
+    const clientHeight = scrollDiv.clientHeight;
+    const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+    if (isAtBottom) {
+    }
   }
 }
