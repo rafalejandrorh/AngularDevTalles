04b7e542ebaebba8a68172d4da0f0a1646537dd6
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-20 16:22:37 -0400

    Maintain scroll position

--- 02-gifs-app/src/app/gifs/components/gif-list/random-list/random-list.component.ts
+++ 02-gifs-app/src/app/gifs/components/gif-list/random-list/random-list.component.ts
@@ -1,25 +1,38 @@
-import { Component, ElementRef, inject, input, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, input, viewChild } from '@angular/core';
 import { Gif } from 'src/app/gifs/interfaces/gif.interface';
 import { GifService } from 'src/app/gifs/services/gif.service';
+import { ScrollStateService } from 'src/app/gifs/services/ScrollState.service';
 
 @Component({
   selector: 'gif-random-list',
-  imports: [],
   templateUrl: './random-list.component.html',
 })
-export class RandomListComponent { 
+export class RandomListComponent implements AfterViewInit{ 
 
   gifService = inject(GifService);
+  scrollStateService = inject(ScrollStateService);
   gifsGroup = input.required<Gif[][]>();
   scrollDivRef = viewChild<ElementRef>('groupDiv');
+
+  ngAfterViewInit(): void {
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if(!scrollDiv) return;
+
+    scrollDiv.scrollTop = this.scrollStateService.trendingScrollState();
+    scrollDiv.addEventListener('scroll', this.onScroll.bind(this));
+  }
 
   onScroll(event: Event) {
     const scrollDiv = this.scrollDivRef()?.nativeElement;
     if(!scrollDiv) return;
+
     const scrollTop = scrollDiv.scrollTop;
     const scrollHeight = scrollDiv.scrollHeight;
     const clientHeight = scrollDiv.clientHeight;
     const isAtBottom = scrollTop + clientHeight + 300 >= scrollHeight;
+
+    this.scrollStateService.trendingScrollState.set(scrollTop);
+
     if (isAtBottom) {
       this.loadMoreGifs();
     }
