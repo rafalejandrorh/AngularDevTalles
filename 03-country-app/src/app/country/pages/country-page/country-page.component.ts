80734186a744098f4becc7a6874b86cf4731217c
Rafael Rivero <rafael.rivero@simpledatacorp.com>
2025-05-21 20:56:26 -0400

    Not Found Component

--- 03-country-app/src/app/country/pages/country-page/country-page.component.ts
+++ 03-country-app/src/app/country/pages/country-page/country-page.component.ts
@@ -3,10 +3,11 @@ import { inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { CountryService } from '../../services/country.service';
+import { NotFoundComponent } from "../../../shared/components/not-found/not-found.component";
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [NotFoundComponent],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent { 
