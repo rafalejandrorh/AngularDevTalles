733f40f2734fafed4d2b576a504f94edf80d983d
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-03 11:56:12 -0400

    Refactor ByRegionPage to use signal for query management and integrate country resources

--- 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { ListComponent } from "../../components/list/list.component";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CountryService } from '../../services/country.service';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-by-region-page',
@@ -9,8 +12,16 @@ import { ListComponent } from "../../components/list/list.component";
 })
 export class ByRegionPageComponent { 
   
-  onSearch(value: string) {
-    console.log(value);
-  }
+  countryService = inject(CountryService)
+  query = signal<string>('');
+
+  // rxResource is Experimental
+  countryResources = rxResource({
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+      if(!request.query) return of([]);
+      return this.countryService.searchByRegion(this.query());
+    }
+  });
   
 }
