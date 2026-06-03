3a50bcaab6a85248730a661fbb05b4e7a4846512
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-03 16:00:32 -0400

    Refactor logging messages in CountryService for clarity and consistency

--- 03-country-app/src/app/country/services/country.service.ts
+++ 03-country-app/src/app/country/services/country.service.ts
@@ -20,11 +20,11 @@ export class CountryService {
   searchByCapital(capital: string): Observable<Country[]> {
     const query = capital.toLowerCase();
     if(this.queryCacheCapital.has(query)) {
-      console.log('Returning cached result for capital:', capital);
+      console.log('Returning cached result by capital:', capital);
       return of(this.queryCacheCapital.get(query)!);
     }
 
-    console.log('Fetching from API for capital:', capital);
+    console.log('Fetching from API by capital:', capital);
     const url = `${this.restCountriesUrl}/capital/${query}`
     return this.http.get<RestCountryResponse[]>(url).pipe(
       map((response) => CountryMapper.mapRestCountriesToCountriesArray(response)),
@@ -37,16 +37,16 @@ export class CountryService {
     );
   }
 
-  searchByCountry(country: string) {
+  searchByCountry(country: string): Observable<Country[]> {
     const query = country.toLowerCase();
     if(this.queryCacheCountry.has(query)) {
-      console.log('Returning cached result for country:', country);
+      console.log('Returning cached result by country:', country);
       return of(this.queryCacheCountry.get(query)!).pipe(
         delay(3000) // Simulate a delay of 3 second
       );
     }
 
-    console.log('Fetching from API for country:', country);
+    console.log('Fetching from API by country:', country);
     const url = `${this.restCountriesUrl}/name/${query}`
     return this.http.get<RestCountryResponse[]>(url).pipe(
       map((response) => CountryMapper.mapRestCountriesToCountriesArray(response)),
@@ -54,13 +54,15 @@ export class CountryService {
       delay(3000), // Simulate a delay of 3 second
       catchError((error) => {
         console.log(error);
-        console.error('Error fetching countries by name:', error);
+        console.error('Error fetching countries by country:', error);
         return throwError(() => new Error(`No se encontraron Países con esa búsqueda: ${country}`));
       })
     );
   }
 
   searchByAlphaCode(code: string) {
+    const query = code;
+    console.log('Fetching from API by alpha code:', code);
     const url = `${this.restCountriesUrl}/alpha/${code}`
     return this.http.get<RestCountryResponse[]>(url).pipe(
       map((response) => CountryMapper.mapRestCountriesToCountriesArray(response)),
@@ -72,14 +74,14 @@ export class CountryService {
     );
   }
 
-  searchByRegion(region: string) {
+  searchByRegion(region: string): Observable<Country[]> {
     const query = region.toLowerCase();
     if(this.queryCacheRegion.has(query)) {
-      console.log('Returning cached result for region:', region);
+      console.log('Returning cached result by region:', region);
       return of(this.queryCacheRegion.get(query)!);
     }
 
-    console.log('Fetching from API for region:', region);
+    console.log('Fetching from API by region:', region);
     const url = `${this.restCountriesUrl}/region/${query}`
     return this.http.get<RestCountryResponse[]>(url).pipe(
       map((response) => CountryMapper.mapRestCountriesToCountriesArray(response)),
