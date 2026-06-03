fbce6d020795555c92a37b11a6ca50b969e05c4c
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-03 14:43:07 -0400

    Refactor ByRegionPage to replace search input with region selection buttons and update country resource handling

--- 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,27 +1,36 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { SearchInputComponent } from "../../components/search-input/search-input.component";
 import { ListComponent } from "../../components/list/list.component";
 import { rxResource } from '@angular/core/rxjs-interop';
 import { CountryService } from '../../services/country.service';
-import { of } from 'rxjs';
+import { firstValueFrom, of } from 'rxjs';
+import { Region, REGIONS } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
-  imports: [SearchInputComponent, ListComponent],
+  imports: [ListComponent],
   templateUrl: './by-region-page.component.html'
 })
 export class ByRegionPageComponent { 
   
+  public readonly regions: readonly Region[] = REGIONS;
+
+  selectedRegion = signal<Region | null>(null);
   countryService = inject(CountryService)
   query = signal<string>('');
 
-  // rxResource is Experimental
-  countryResources = rxResource({
+  // resource is Experimental
+  countryResources = resource({
     request: () => ({ query: this.query() }),
-    loader: ({ request }) => {
-      if(!request.query) return of([]);
-      return this.countryService.searchByRegion(this.query());
+    loader: async({ request }) => {
+      if(!request.query) return [];
+      return await firstValueFrom(this.countryService.searchByRegion(this.query()));
     }
   });
+
+  selectRegion(region: Region) {
+    this.selectedRegion.set(region);
+    this.query.set(region);
+  }
   
 }
