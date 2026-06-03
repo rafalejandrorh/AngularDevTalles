35be91cb91f8a86023b5976b8182b0563bf54dff
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-03 14:09:19 -0400

    Fix capital mapping to handle undefined values in CountryMapper

--- 03-country-app/src/app/country/mappers/country.mapper.ts
+++ 03-country-app/src/app/country/mappers/country.mapper.ts
@@ -9,7 +9,7 @@ export class CountryMapper {
         flag: restCountry.flag,
         flagSvg: restCountry.flags.svg,
         name: restCountry.translations['spa'].common ?? 'No Spanish Name Country',
-        capital: restCountry.capital.join(','),
+        capital: restCountry.capital?.join(',') ?? 'No Capital Country',
         population: restCountry.population,
         region: restCountry.region,
         subregion: restCountry.subregion,
