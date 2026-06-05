0a9354c00cee0f94ddb1b80de25939efff367a51
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-05 11:53:36 -0400

    Add basic page layout and implement string transformation pipes

--- 04-pipes-app/src/app/pages/basic-pages/basic-page.ts
+++ 04-pipes-app/src/app/pages/basic-pages/basic-page.ts
@@ -1,9 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LowerCasePipe, UpperCasePipe, TitleCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class BasicPage {}
+export default class BasicPage {
+
+  nameLower = signal('Rafael Rivero');
+  nameUpper = signal('RAFAEL RIVERO');
+  nameDisaster = signal('RaFaEl rIvErO');
+
+}
