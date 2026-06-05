53670cabb4754cf5cb272e350d06da53ea362551
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-05 08:57:27 -0400

    Refactor ByRegionPageComponent to update region selection logic and integrate query parameter handling

--- 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ 03-country-app/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, resource, signal } from '@angular/core';
+import { Component, inject, linkedSignal, resource, signal } from '@angular/core';
 import { ListComponent } from "../../components/list/list.component";
 import { rxResource } from '@angular/core/rxjs-interop';
 import { firstValueFrom, of } from 'rxjs';
 import { CountryService } from '../../services/country.service';
 import { SearchRegionButtonComponent } from "../../components/search-region-button/search-region-button.component";
+import { ActivatedRoute, Router } from '@angular/router';
+import { Region, REGIONS } from '../../interfaces/region.type';
 
 @Component({
   selector: 'app-by-region-page',
@@ -12,16 +14,29 @@ import { SearchRegionButtonComponent } from "../../components/search-region-butt
 })
 export class ByRegionPageComponent { 
 
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
   countryService = inject(CountryService)
-  query = signal<string>('');
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+  selectedRegion = linkedSignal<Region | null>(() => validateQueryParam(this.queryParam));
 
   // resource is Experimental
   countryResources = resource({
-    request: () => ({ query: this.query() }),
+    request: () => ({ region: this.selectedRegion() }),
     loader: async({ request }) => {
-      if(!request.query) return [];
-      return await firstValueFrom(this.countryService.searchByRegion(this.query()));
+      if(!request.region) return of([]);
+      this.router.navigate(['/country/by-region'], {
+        queryParams: { region: request.region },
+      });
+      return await firstValueFrom(this.countryService.searchByRegion(request.region));
     }
   });
   
+}
+
+function validateQueryParam(queryParam: string): Region {
+  if (REGIONS.includes(queryParam as Region)) {
+    return REGIONS.filter(region => region === queryParam)[0] as Region;
+  }
+  return 'Americas';
 }
