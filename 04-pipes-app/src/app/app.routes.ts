69a716ad3d0c03c9ba0e6645cd3adc1a1dd0b460
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-05 11:36:11 -0400

    Add Routes

--- 04-pipes-app/src/app/app.routes.ts
+++ 04-pipes-app/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'basic',
+        title: 'Basic Pipes',
+        loadComponent: () => import('./pages/basic-pages/basic-page').then(m => m.default)
+    },
+    {
+        path: 'numbers',
+        title: 'Numbers Pipes',
+        loadComponent: () => import('./pages/numbers-page/numbers-page').then(m => m.default)
+    },
+    {
+        path: 'uncommon',
+        title: 'Uncommon Pipes',
+        loadComponent: () => import('./pages/uncommon-page/uncommon-page').then(m => m.default)
+    },
+    {
+        path: 'custom',
+        title: 'Custom Pipes',
+        loadComponent: () => import('./pages/custom-page/custom-page').then(m => m.default)
+    },
+    {
+        path: '**',
+        redirectTo: 'basic'
+    }
+];
